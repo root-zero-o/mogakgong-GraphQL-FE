603474aa69266e435c62ff4165358ac9e89985d5
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-07-06 14:13:01 -0400

    feat: timeChecker view (50%)

--- pages/main.tsx
+++ pages/main.tsx
@@ -7,40 +7,40 @@ const main = () => {
   return (
     <div className="wrapper h-full bg-ivory overflow-hidden relative">
       <div className="flex flex-col mt-8 text-25">
-        <span>안녕하세요 영근님!</span>
-        <span>오늘도 열공해볼까요?</span>
+        <h3>안녕하세요 영근님!</h3>
+        <h3>오늘도 열공해볼까요?</h3>
       </div>
 
       <div
         className="circle shadow-2xl absolute left-[15px] top-[20%] w-[170px] h-[170px] bg-green_light text-4xl"
         onClick={() => router.push("/studyList")}
       >
-        <span>나의</span>
-        <span>스터디</span>
+        <h1>나의</h1>
+        <h1>스터디</h1>
       </div>
 
       <div
         className="circle shadow-2xl absolute right-[-40px] top-[30%] w-[220px] h-[220px] bg-green_mid text-4xl"
         onClick={() => router.push("/timeCheck")}
       >
-        <span>공부시간</span>
-        <span>측정</span>
+        <h1>공부시간</h1>
+        <h1>측정</h1>
       </div>
 
       <div
         className="circle shadow-2xl absolute left-[-40px] bottom-[10%] w-[280px] h-[280px] bg-green_deep text-4xl"
         onClick={() => router.push("/todoList")}
       >
-        <span>나의 할일</span>
-        <span>관리하기</span>
+        <h1>나의 할일</h1>
+        <h1>관리하기</h1>
       </div>
 
       <div
         className="circle shadow-2xl absolute right-[-10px] bottom-[-20px] w-[180px] h-[180px] bg-green_mid text-4xl"
         onClick={() => router.push("/myPage")}
       >
-        <span>마이</span>
-        <span>페이지</span>
+        <h1>마이</h1>
+        <h1>페이지</h1>
       </div>
     </div>
   );
--- pages/timeCheck.tsx
+++ pages/timeCheck.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const timeCheck = () => {
-  return <div>timeCheck</div>;
+  const [flag, setFlag] = useState(false);
+  const onStartHandler = () => {
+    setFlag((value) => !value);
+  };
+  return (
+    <div className="wrapper h-full bg-green_mid overflow-scroll relative">
+      <div className="flex flex-col items-center justify-center">
+        <div className="mt-12 ">
+          <h1>공부시간 측정</h1>
+          <h2 className="mt-10 text-color text-white ">이번주 나의 공부시간</h2>
+        </div>
+        <div className="flex flex-col items-center justify-center w-[260px] h-[129px] bg-white rounded-3xl mt-5">
+          <div className="h-[100%] flex flex-col items-center justify-center">
+            <h1>1시간 1분 1초</h1>
+          </div>
+          <h2 className="text-20 text-red_deep -mt-10">목표시간 초과 달성</h2>
+        </div>
+        <h2 className="mt-10 text-color text-white">오늘의 공부시간 측정</h2>
+        <div className="flex flex-col items-center justify-center w-[320px] h-[180px] bg-white rounded-3xl mt-5">
+          <div className="h-[40%] flex flex-col items-center justify-start mt-6">
+            <h1>01:01:01</h1>
+          </div>
+          <div className="h-[40%] flex flex-col items-center justify-center ">
+            {!flag ? (
+              <div
+                className="circle w-[70px] h-[70px] bg-red_light shadow-md"
+                onClick={onStartHandler}
+              >
+                <h3 className="text-white">Start</h3>
+              </div>
+            ) : (
+              <div
+                className="circle w-[70px] h-[70px] bg-green_deep shadow-md"
+                onClick={onStartHandler}
+              >
+                <h3 className="text-white">Stop</h3>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default timeCheck;
