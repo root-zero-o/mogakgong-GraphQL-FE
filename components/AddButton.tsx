ae5cda1a76ff89d0672c5723f0dab8a0ea9742f0
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-07-08 05:02:39 -0400

    feat: addButton 컴포넌트

---
+++ components/AddButton.tsx
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const AddButton = (props: { link: string }) => {
+  const router = useRouter();
+  return (
+    <div
+      className="circle fixed right-5 bottom-5 w-[80px] h-[80px] bg-green_deep shadow-2xl hover:bg-green_mid"
+      onClick={() => router.push(`/${props.link}`)}
+    >
+      <h1 className="mt-2">+</h1>
+    </div>
+  );
+};
+
+export default AddButton;
--- pages/studyList.tsx
+++ pages/studyList.tsx
@@ -1,9 +1,11 @@
 import StudyListContainer from "../components/StudyListContainer";
+import AddButton from "../components/AddButton";
 
 const studyList = () => {
   return (
-    <div className="wrapper h-full bg-ivory overflow-scroll">
+    <div className="wrapper h-full bg-ivory overflow-scroll relative">
       <h1 className="mt-10">나의 스터디</h1>
+      <AddButton link="login" />
       <StudyListContainer />
     </div>
   );
