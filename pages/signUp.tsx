205afa58958b43c8c361c90405ce233accabd1ff
영근 <rootzero17@gmail.com>
2022-07-21 00:08:54 +0900

    react-hook-form 없이 유효성 검사

--- pages/signUp.tsx
+++ pages/signUp.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Seo from "../components/Seo";
-import { User } from "../typings";
-import { RegCheck } from "../lib/RegCheck";
+import SignupInput from "../components/SignupInput";
+import BackButton from "../components/BackButton";
 
 const signUp = () => {
   return (
     <div className="wrapper bg-green_mid py-8">
+      <BackButton />
       <Seo title="회원가입" />
       <div className="circle z-0 absolute w-96 h-96 right-[-80px] top-[-40px] bg-green_light" />
       <div className="circle z-0 absolute w-96 h-96 left-[-100px] bottom-4 bg-ivory" />
@@ -20,43 +21,13 @@ const signUp = () => {
         </label>
         <div className="z-40 flex flex-col justify-center items-center w-full">
           <input id="imgInput" type="file" className="hidden" />
-          <input
-            type="email"
-            className="invalid:border-2 textInput"
-            placeholder="이메일 아이디"
-          />
         </div>
-        <div className="z-40 flex flex-col justify-center items-center w-full ">
-          <input
-            type="text"
-            className="textInput"
-            placeholder="닉네임(1~4글자)"
-          />
-        </div>
-        <div className="z-40 flex flex-col justify-center items-center w-full">
-          <input type="password" className="textInput" placeholder="비밀번호" />
-        </div>
-        <div className="z-40 flex flex-col justify-center items-center w-full">
-          <input
-            type="password"
-            className="textInput"
-            placeholder="비밀번호 확인"
-          />
-        </div>
-        <div className="z-40 flex flex-col justify-center items-center w-full">
-          <input
-            type="number"
-            className="textInput"
-            placeholder="목표 공부시간"
-          />
-        </div>
-        <div className="z-40 flex flex-col justify-center items-center w-full">
-          <input
-            type="text"
-            className="textInput"
-            placeholder="각오 한 마디(최대 20자)"
-          />
-        </div>
+        <SignupInput placeholder="이메일 아이디" type="email" />
+        <SignupInput placeholder="닉네임(1~4글자)" type="text" />
+        <SignupInput placeholder="비밀번호" type="password" />
+        <SignupInput placeholder="비밀번호 확인" type="password" />
+        <SignupInput placeholder="목표 공부시간" type="number" />
+        <SignupInput placeholder="각오 한 마디(최대 20자)" type="text" />
       </form>
     </div>
   );
